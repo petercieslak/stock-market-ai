51b93757916b327af6cfed4cd8859239e6c4f8a0
piotrc <piotr.m.cieslak@gmail.com>
2023-04-26 14:22:03 +0200

    Preparing data for LSTM

--- main.py
+++ main.py
@@ -15,3 +15,30 @@ endDate = date(2020, 10, 5)
 StockData = get_history(symbol="SBIN", start=startDate, end=endDate)
 print(StockData.shape)
 StockData.head()
+
+# split into samples
+X_samples = list()
+y_samples = list()
+
+NumerOfRows = len(X)
+TimeSteps = 10  # next day's Price Prediction is based on last how many past day's prices
+
+# Iterate thru the values to create combinations
+for i in range(TimeSteps, NumerOfRows, 1):
+    x_sample = X[i - TimeSteps:i]
+    y_sample = X[i]
+    X_samples.append(x_sample)
+    y_samples.append(y_sample)
+
+################################################
+# Reshape the Input as a 3D (number of samples, Time Steps, Features)
+X_data = np.array(X_samples)
+X_data = X_data.reshape(X_data.shape[0], X_data.shape[1], 1)
+print('\n#### Input Data shape ####')
+print(X_data.shape)
+
+# We do not reshape y as a 3D data  as it is supposed to be a single column only
+y_data = np.array(y_samples)
+y_data = y_data.reshape(y_data.shape[0], 1)
+print('\n#### Output Data shape ####')
+print(y_data.shape)
